60668d316c83ac18fbdc21f06198b0e86279eade
Jonny Preuss <jonny.p.hh@gmail.com>
2025-06-01 14:11:03 +0200

    edit config for background file

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 from openpyxl.utils import get_column_letter
 
 # --- CONFIGURATION ---
-EXCEL_FILE = "data/Top_1000_verbs_French.xlsx"
+EXCEL_FILE = "data/Top_1000_verbs_French_USE.xlsx"
 START_ROW = 3
 VERB_COL = "B"
-STATUS_COL = "AE"
+STATUS_COL = "AW"
 # CONJUGATION_COLS = list(map(chr, range(ord("C"), ord("AV"))))  # Columns C to AU
 CONJUGATION_COLS = [get_column_letter(i) for i in range(3, 48)]  # Columns C (3) to AU (47)
